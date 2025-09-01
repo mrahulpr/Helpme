404d332b8b2af5ceda594bfb3b3ae6e0e559f7f0
Rahul P R <88572526+mrahulpr@users.noreply.github.com>
2025-09-01 16:24:04 +0530

    Update __init__.py

--- ZarinaBot/modules/sql/__init__.py
+++ ZarinaBot/modules/sql/__init__.py
@@ -2,7 +2,11 @@ from ZarinaBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.dialects.postgresql import BIGINT
+from sqlalchemy import Integer
 
+# Force Integer to behave like BIGINT in Postgres
+Integer().with_variant(BIGINT(), "postgresql")
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
